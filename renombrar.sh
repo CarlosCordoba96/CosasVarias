eed5f81ff5501c2fc8ac3f11b49d4abf15f7de4d
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2017-05-18 23:59:39 +0200

    Renombramiento aleatorio

--- renombrar.sh
+++ renombrar.sh
@@ -1,16 +1,20 @@
+
 #!/bin/bash
 ################################
 # renombrar.sh                 #
 # script renombramiento masivo #
 # numerando desde 1            #
 ################################
-cont=0
-nombre="Cancion_"
-for picture in `ls **.mp3`
+for FILE in *.mp3 ;
 do
-((cont=$RANDOM))
-nuevonombre=$nombre$cont
-echo "Renombrando... $picture"
-echo "a $nuevonombre.pdf"
-mv $picture $nuevonombre.pdf
+    NEWFILE=`echo $FILE | sed 's/ /_/g'`
+    mv "$FILE" $NEWFILE
+ done
+ cont=0
+ nombre="Cancion_"
+ for picture in `ls **.mp3`
+ do
+ ((cont=$RANDOM))
+ nuevonombre=$nombre$cont
+ mv $picture $nuevonombre.mp3
 done
